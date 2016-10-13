0a06ffba5adda2477954e3d7cb02d4ed7b987ac5
Lucas Rodrigues <lrodrigues@allin.com.br>
2016-10-13 15:43:54 -0300

    Atualização de documentação

--- AllInMobileLibrary/pushnotifications-library/src/main/java/br/com/allin/mobile/pushnotification/interfaces/AllInDelegate.java
+++ AllInMobileLibrary/pushnotifications-library/src/main/java/br/com/allin/mobile/pushnotification/interfaces/AllInDelegate.java
@@ -4,13 +4,12 @@ package br.com.allin.mobile.pushnotification.interfaces;
  * @author lucasbrsilva
  *
  * Interface that returns the push action text
- *
  */
 public interface AllInDelegate {
     /**
      * Method call that calls the push action
      *
-     * @param action Ação do push
+     * @param action Push action
      */
     void onAction(String action);
 }
