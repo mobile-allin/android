c33f16353fc97b8a7e4e723c0c28a88286b7f316
Lucas Rodrigues <lucasbrsilva@gmail.com>
2020-10-29 19:51:35 -0300

    Adicionando data na lista de persistência

--- AllInMobileLibrary/pushnotifications-library/src/main/java/br/com/allin/mobile/pushnotification/helper/ListPersistence.java
+++ AllInMobileLibrary/pushnotifications-library/src/main/java/br/com/allin/mobile/pushnotification/helper/ListPersistence.java
@@ -1,13 +1,17 @@
 package br.com.allin.mobile.pushnotification.helper;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.allin.mobile.pushnotification.entity.allin.AIValues;
 
 public class ListPersistence {
     public static String getMD5(String nameList, List<AIValues> columnsAndValues) {
+        String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         String json = columnsAndValues.toString();
-        String complete = "List: " + nameList + " Json: " + json;
+        String complete = "List: " + nameList + " Json: " + json + " Date: " + date;
 
         return Util.md5(complete);
     }
